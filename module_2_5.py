4887468a13a6a635ddd7d06737b135b6cdc8fe2f
RomanTMB68 <medoedtmb@gmail.com>
2024-07-05 16:19:05 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,23 @@
+def get_matrix(n, m, value):
+    matrix = []
+
+    for i in range(n):
+        listF = []
+
+        for g in range(m):
+            listF.append(value)
+        matrix.append(listF)
+
+    return matrix
+
+
+result1 = get_matrix(2, 2, 10)
+
+result2 = get_matrix(3, 5, 42)
+
+result3 = get_matrix(4, 2, 13)
+
+
+print(result1)
+print(result2)
+print(result3)
